6c4a1e102a138178069249a4b868296360baba9c
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-07-01 20:47:35 -0700

    beginning work

--- js/application.js
+++ js/application.js
@@ -1,9 +1,37 @@
 (function(){
 
-	if(!window.TMNT) {window.TMNT = {}}	//Create Namespace
-
+	if(!window.JS) {window.JS = {}}	//Create Namespace
+	
+	JS.Carousel = {
+		this: JS.Carousel,
+		viewport: 2,
+		width:200,
+		getSlots : function(){
+			var carousel = document.getElementById("carousel");
+			return carousel.getElementsByTagName("li").length;
+		},
+		setCarouselWidth : function(){
+			var width = this.viewport * this.width;
+			var carousel = document.getElementById("carousel");
+			carousel.style.width = width + "px";
+		},
+		onLeftClick : function(){
+			console.log("do left");
+		},
+		onRightClick : function(){
+			console.log("do right");
+		},
+		animate : function(){
+			var viewPort = this.viewport;
+			var slots = this.getSlots();
+			this.setCarouselWidth();
+			this.onLeftClick();
+			this.onRightClick();
+		}
+	}
+	
 })();
 		
 $(document).ready(function(){
-	
+	JS.Carousel.animate();
 });
