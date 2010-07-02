a4c4bbea58646a925d8cc3a15abeb6b511768e9e
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-07-02 13:28:51 -0700

    added left-click animation

--- js/application.js
+++ js/application.js
@@ -3,9 +3,23 @@
 	if(!window.JS) {window.JS = {}}	//Create Namespace
 	
 	JS.Carousel = {
+		
 		this: JS.Carousel,
 		viewport: 2,
-		width:200,
+		width :200,
+		increment: 1,
+		
+		move : function(direction){
+			var slots = document.getElementById("slots");
+			this.increment += 50;
+			if(!(this.increment > 200)){
+				slots.style.left = this.increment + 50 + "px";
+				setTimeout("JS.Carousel.move()",20)
+				console.log(this.increment);
+			} else {
+				console.log("stopped");
+			}
+		},
 		getSlots : function(){
 			var carousel = document.getElementById("carousel");
 			return carousel.getElementsByTagName("li").length;
@@ -16,10 +30,16 @@
 			carousel.style.width = width + "px";
 		},
 		onLeftClick : function(){
-			console.log("do left");
+			var lClick = document.getElementById("leftClick");
+			lClick.onclick = function(){
+				JS.Carousel.move("left");
+			}
 		},
 		onRightClick : function(){
-			console.log("do right");
+			var rClick = document.getElementById("rightClick");
+			rClick.onclick = function(){
+				JS.Carousel.move("right");
+			}
 		},
 		animate : function(){
 			var viewPort = this.viewport;
